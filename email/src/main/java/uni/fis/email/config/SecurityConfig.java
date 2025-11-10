b0f35d6d8db00661f7a06e0af014a2468085fb32
Giovanny Sierra <giosreina@gmail.com>
2025-11-10 10:30:28 -0500

    eliminación de UserDetails dentro de clase SecurityConfig.java, para el inicio de la validación por tokens y no por memoria

--- email/src/main/java/uni/fis/email/config/SecurityConfig.java
+++ email/src/main/java/uni/fis/email/config/SecurityConfig.java
@@ -5,12 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,25 +22,13 @@ public class SecurityConfig {
                 .requestMatchers("/api/email/**").authenticated()
                 .anyRequest().authenticated()
             )
-            .httpBasic(basic -> {})
             .sessionManagement(session -> 
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             );
         
         return http.build();
     }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-            .username("admin")
-            .password(passwordEncoder().encode("admin123"))
-            .roles("ADMIN")
-            .build();
-        
-        return new InMemoryUserDetailsManager(user);
-    }
-
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
