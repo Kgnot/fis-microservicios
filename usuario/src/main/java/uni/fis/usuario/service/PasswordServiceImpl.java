458aeea5499d490604d7c836c7fe9a5f65f9ad88
Kgnot <hricaurtem@proton.me>
2025-11-18 18:28:07 -0500

    Validaciones mejoradas

--- usuario/src/main/java/uni/fis/usuario/service/PasswordServiceImpl.java
+++ usuario/src/main/java/uni/fis/usuario/service/PasswordServiceImpl.java
@@ -1,5 +1,6 @@
 package uni.fis.usuario.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import uni.fis.usuario.entity.PasswordEntity;
 import uni.fis.usuario.repository.PasswordRepository;
@@ -12,16 +13,18 @@ public class PasswordServiceImpl implements PasswordService {
 
     private final PasswordRepository repository;
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public PasswordServiceImpl(PasswordRepository repository, UserRepository userRepository) {
+    public PasswordServiceImpl(PasswordRepository repository,
+                               UserRepository userRepository) {
         this.repository = repository;
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Override
     public boolean validate(String email, String password) {
-
-        // Buscamos el usuario primero
+        // Buscamos el usuario
         var usuario = userRepository.findByEmail(email);
         if (usuario.isEmpty()) {
             return false;
@@ -30,11 +33,8 @@ public class PasswordServiceImpl implements PasswordService {
         // Buscamos la última contraseña
         Optional<PasswordEntity> ultima = repository.findMasRecienteById(usuario.get().getId());
 
-        boolean isValid = ultima
-                .map(passwordEntity -> passwordEntity.getChars().equals(password))
+        return ultima
+                .map(passwordEntity -> passwordEncoder.matches(password, passwordEntity.getChars()))
                 .orElse(false);
-
-        System.out.println("Resultado final de validación: " + isValid);
-        return isValid;
     }
 }
