5b8f6f869a1447a185104ec13be5b46eb5f0873c
Kgnot <hricaurtem@proton.me>
2025-11-18 17:05:40 -0500

    Se creo la semejanzas de respuestas

--- usuario/src/main/java/uni/fis/usuario/controller/ValidateController.java
+++ usuario/src/main/java/uni/fis/usuario/controller/ValidateController.java
@@ -6,8 +6,9 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import uni.fis.usuario.dto.request.TokenRequest;
 import uni.fis.usuario.dto.request.UserValidateRequest;
+import uni.fis.usuario.dto.response.ApiResponse;
+import uni.fis.usuario.dto.response.TokenResponse;
 import uni.fis.usuario.service.PasswordService;
 import uni.fis.usuario.service.UserService;
 
@@ -20,21 +21,20 @@ public class ValidateController {
     private final PasswordService passwordService;
 
     @PostMapping("/auth/verify")
-    public ResponseEntity<?> validateUser(@RequestBody UserValidateRequest userValidateRequest) {
+    public ResponseEntity<ApiResponse<TokenResponse>> validateUser(@RequestBody UserValidateRequest userValidateRequest) {
         var validate = passwordService.validate(userValidateRequest.id(), userValidateRequest.password());
         if (validate) {
             var user = userService.findById(userValidateRequest.id());
-            if (user.isEmpty()) {
-                return ResponseEntity
-                        .of(ProblemDetail.forStatus(403))
-                        .build();
-            }
-            return ResponseEntity.ok(
-                    new TokenRequest(
-                            user.get().id(),
-                            user.get().idRol()
+            return user.map(userDto -> ResponseEntity.ok(
+                    ApiResponse.success("Usuario verificado",
+                            new TokenResponse(
+                                    userDto.id(),
+                                    userDto.idRol()
+                            )
                     )
-            );
+            )).orElseGet(() -> ResponseEntity
+                    .of(ProblemDetail.forStatus(403))
+                    .build());
         }
         return new ResponseEntity<>(HttpStatusCode.valueOf(403));
     }
