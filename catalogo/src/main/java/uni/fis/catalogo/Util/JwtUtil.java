13ce9ac26c4555c221b0e1f328ef47233bf8f0fe
Giovanny Sierra <giosreina@gmail.com>
2025-11-19 07:47:48 -0500

    Creacion de una utilidad para extraer informacion de tokens, y alteracion del archivo pom.xml para el manejo de jwt

---
+++ catalogo/src/main/java/uni/fis/catalogo/Util/JwtUtil.java
@@ -0,0 +1,39 @@
+package uni.fis.catalogo.Util;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+
+@Deprecated(since = "1.1", forRemoval = true)
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "hjq&Iou4%7POdr$lX9s@1yTz3!vBnLpQ";
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey()) 
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+}
