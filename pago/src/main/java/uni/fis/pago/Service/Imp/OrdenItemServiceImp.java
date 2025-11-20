c30a14b1c9284a6808f888f4dcf75c0baadde0ac
Giovanny Sierra <giosreina@gmail.com>
2025-11-20 14:54:13 -0500

    Servicio para el manejo de las ordenes de items finalizado y funcional

--- pago/src/main/java/uni/fis/pago/Service/Imp/OrdenItemServiceImp.java
+++ pago/src/main/java/uni/fis/pago/Service/Imp/OrdenItemServiceImp.java
@@ -7,6 +7,7 @@ import uni.fis.pago.Entity.OrdenItem;
 import uni.fis.pago.Model.OrdenItemDTO.*;
 import uni.fis.pago.Repository.OrdenItemRepository;
 import uni.fis.pago.Service.Interfaces.OrdenItemService;
+import uni.fis.pago.Exceptions.Exceptions;
 
 @Service
 public class OrdenItemServiceImp implements OrdenItemService{
@@ -27,9 +28,23 @@ public class OrdenItemServiceImp implements OrdenItemService{
     @Override
     public void eliminarOrdenItem(Integer id){
         if(!ordenItemRepository.existsById(id)){
-            throw new
+            throw new Exceptions("Orden de item con el id "+ id +" no fue encontrado", "ORDEN_ITEM_NOT_FOUND" );
         }
         ordenItemRepository.deleteById(id);
+    }
+    @Override
+    public OrdenItemResponse consultarOrdenItem(Integer idOrdenItem){
+        OrdenItem ordenItem = ordenItemRepository.findById(idOrdenItem).
+        orElseThrow(() -> new Exceptions("Orden de item con el id "+ idOrdenItem +" no fue encontrado", "ORDEN_ITEM_NOT_FOUND"));
+        OrdenItemResponse ordenItemResponse = OrdenItemResponse.builder()
+                                              .id(ordenItem.getId())
+                                              .id_item(ordenItem.getId_item())
+                                              .cantidad(ordenItem.getCantidad())
+                                              .valor_unitario(ordenItem.getValor_unitario())
+                                              .id_orden_compra(ordenItem.getId_orden_compra())
+                                              .subtotal(ordenItem.getSubtotal())
+                                              .build();
+        return ordenItemResponse;
     }
 
 }
