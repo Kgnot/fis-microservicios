8405c91a21f94082afc47cddf619df12ced74c27
Giovanny Sierra <giosreina@gmail.com>
2025-11-20 12:31:25 -0500

    Creacion del repositorio de OrdenCompra

---
+++ pago/src/main/java/uni/fis/pago/Repository/OrdenCompraRepository.java
@@ -0,0 +1,11 @@
+package uni.fis.pago.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import uni.fis.pago.Entity.OrdenCompra;
+
+@Repository
+public interface OrdenCompraRepository extends JpaRepository<OrdenCompra, Integer> {
+    OrdenCompra findById_pago(Integer idPago);
+}
