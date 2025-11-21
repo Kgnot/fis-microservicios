eb37ea613aecb974cd9035cba822fe562f2ebc49
Giovanny Sierra <giosreina@gmail.com>
2025-11-21 07:06:35 -0500

    Modificacion del repositorio de ordenes de items para verificar la existencia de ordenes de items por la orden de compra

--- pago/src/main/java/uni/fis/pago/Repository/OrdenItemRepository.java
+++ pago/src/main/java/uni/fis/pago/Repository/OrdenItemRepository.java
@@ -1,13 +1,14 @@
 package uni.fis.pago.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import uni.fis.pago.Entity.OrdenItem;
 
-import java.util.List;
-
 @Repository
 public interface OrdenItemRepository extends JpaRepository<OrdenItem, Integer> {
     List<OrdenItem> findById_orden_compra(Integer idOrdenCompra);
+    boolean existsById_orden_compra(Integer idOrdenCompra);
 }
