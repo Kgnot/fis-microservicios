348ce10f5c18e9cb33d10790f3ecd37b2993e899
Giovanny Sierra <giosreina@gmail.com>
2025-11-21 07:08:04 -0500

    Modificacion de la implementacion del servicio del orden de compra para mostrar logs según las funciones que hace

--- pago/src/main/java/uni/fis/pago/Service/Imp/OrdenCompraServiceImp.java
+++ pago/src/main/java/uni/fis/pago/Service/Imp/OrdenCompraServiceImp.java
@@ -1,19 +1,24 @@
 package uni.fis.pago.Service.Imp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.log4j.Log4j2;
 import uni.fis.pago.Entity.OrdenCompra;
-import uni.fis.pago.Model.OrdenCompraDTO.*;
-import uni.fis.pago.Repository.OrdenCompraRepository;
-import uni.fis.pago.Exceptions.Exceptions;
-import uni.fis.pago.Service.Interfaces.OrdenCompraService;
-import uni.fis.pago.Model.OrdenItemDTO.OrdenItemResponse;
 import uni.fis.pago.Entity.OrdenItem;
+import uni.fis.pago.Exceptions.Exceptions;
+import uni.fis.pago.Model.OrdenCompraDTO.OrdenCompraRequest;
+import uni.fis.pago.Model.OrdenCompraDTO.OrdenCompraResponse;
+import uni.fis.pago.Model.OrdenItemDTO.OrdenItemResponse;
+import uni.fis.pago.Repository.OrdenCompraRepository;
 import uni.fis.pago.Repository.OrdenItemRepository;
+import uni.fis.pago.Service.Interfaces.OrdenCompraService;
 
-import java.util.List;
-import java.util.ArrayList;
+@Service
+@Log4j2
 
 public class OrdenCompraServiceImp implements OrdenCompraService{
     @Autowired
@@ -28,17 +33,23 @@ public class OrdenCompraServiceImp implements OrdenCompraService{
                                     .total(ordenCompraRequest.getTotal())
                                     .id_pago(ordenCompraRequest.getId_pago())
                                 .build();
+        log.info("Procesando la información de la orden de compra");
+        ordenCompraRepository.save(ordenCompra);
+        log.info("Orden de compra recibida y aprobada");
         return ordenCompra.getId();
     }
     @Override
     public void eliminarOrdenCompra(Integer idOrdenCompra){
+        log.info("Verificando la orden de compra con el id " + idOrdenCompra);
         if(!ordenCompraRepository.existsById(idOrdenCompra)){
             throw new Exceptions("La Orden de compra con el id " + idOrdenCompra +" no existe","ORDEN_COMPRA_NOT_FOUND");
         }
         ordenCompraRepository.deleteById(idOrdenCompra);
+        log.info("Orden de compra eliminada exitosamente!");
     }
     @Override
     public OrdenCompraResponse consultarOrdenCompra(Integer idOrdenCompra){
+        log.info("Buscando la orden de compra con el id "+idOrdenCompra);
         OrdenCompra ordenCompra = ordenCompraRepository.findById(idOrdenCompra)
                             .orElseThrow(()-> new Exceptions("La Orden de compra con el id " + idOrdenCompra +" no existe","ORDEN_COMPRA_NOT_FOUND"));
         OrdenCompraResponse response = OrdenCompraResponse.builder()
@@ -46,11 +57,16 @@ public class OrdenCompraServiceImp implements OrdenCompraService{
                                         .fecha(ordenCompra.getFecha())
                                         .total(ordenCompra.getTotal())
                                         .id_pago(ordenCompra.getId_pago())
-                                    .build();                    
+                                    .build();   
+        log.info("Orden de compra encontrada exitosamente!");                 
         return response;
     }
     @Override
     public List<OrdenItemResponse> consultarOrdenesItems(Integer idOrdenCompra){
+        log.info("Buscando ordenes de items que pertenecen a la orden de compra");
+        if (!ordenItemRepository.existsById_orden_compra(idOrdenCompra)) {
+            throw new Exceptions("No se han encontrado ordenes de item relacionadas con la orden de compra","ORDEN_ITEM_NOT_FOUND");
+        }    
         List<OrdenItem> ordenesItems = ordenItemRepository.findById_orden_compra(idOrdenCompra);
         List<OrdenItemResponse> response = new ArrayList<>();
         for(OrdenItem ordenItem : ordenesItems){
@@ -64,6 +80,7 @@ public class OrdenCompraServiceImp implements OrdenCompraService{
                                             .build();
             response.add(ordenItemResponse);
         }
+        log.info("Items encontrados exitosamente!");
         return response;
     }
 }
