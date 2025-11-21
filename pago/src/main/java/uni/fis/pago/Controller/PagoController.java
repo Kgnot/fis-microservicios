9dfd86c4d26478378f598ef319f0bb427d9ecd44
Giovanny Sierra <giosreina@gmail.com>
2025-11-21 07:05:14 -0500

    Finalización del controlador para el microservicio, que define el manejo de los pagos, ordenes de compra y ordenes de items

--- pago/src/main/java/uni/fis/pago/Controller/PagoController.java
+++ pago/src/main/java/uni/fis/pago/Controller/PagoController.java
@@ -1,8 +1,11 @@
 package uni.fis.pago.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import uni.fis.pago.Model.OrdenCompraDTO.OrdenCompraRequest;
 import uni.fis.pago.Model.OrdenCompraDTO.OrdenCompraResponse;
+import uni.fis.pago.Model.OrdenItemDTO.OrdenItemRequest;
+import uni.fis.pago.Model.OrdenItemDTO.OrdenItemResponse;
 import uni.fis.pago.Model.PagoDTO.PagoRequest;
 import uni.fis.pago.Model.PagoDTO.PagoResponse;
 import uni.fis.pago.Service.Interfaces.OrdenCompraService;
@@ -47,5 +52,29 @@ public class PagoController{
         OrdenCompraResponse response = ordenCompraService.consultarOrdenCompra(id);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
-
+    @PostMapping("/crearOrdenItem")
+    public ResponseEntity<Integer> crearEntityOrdenItem(@RequestBody OrdenItemRequest ordenItemRequest){
+        Integer ordenItemId = ordenItemService.agregarOrdenItem(ordenItemRequest);
+        return new ResponseEntity<>(ordenItemId, HttpStatus.CREATED);
+    }
+    @GetMapping("/ObtenerOrdenItem/{idOrdenItem}")
+    public ResponseEntity<OrdenItemResponse> obtenerOrdenItemPorId(@PathVariable Integer idOrdenItem){
+        OrdenItemResponse response = ordenItemService.consultarOrdenItem(idOrdenItem);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    @GetMapping("/ObtenerOrdenCompra/{idOrdenCompra}/OrdenesItems")
+    public ResponseEntity<List<OrdenItemResponse>> obtenerOrdenesItemsPorIdOrdenCompra(@PathVariable Integer idOrdenCompra){
+        List<OrdenItemResponse> ordenesDeItems = ordenCompraService.consultarOrdenesItems(idOrdenCompra);
+        return new  ResponseEntity<>(ordenesDeItems, HttpStatus.OK);
+    }
+    @DeleteMapping("/EliminarOrdenCompra/{idOrdenCompra}")
+    public ResponseEntity<String> eliminarOrdenCompraPorId(@PathVariable Integer idOrdenCompra){
+        ordenCompraService.eliminarOrdenCompra(idOrdenCompra);
+        return new ResponseEntity<>("Orden de compra eliminada exitosamente!", HttpStatus.OK);
+    }
+    @DeleteMapping("/EliminarOrdenItem/{idOrdenItem}")
+    public ResponseEntity<String> eliminarOrdenItemPorId(@PathVariable Integer idOrdenItem){
+        ordenItemService.eliminarOrdenItem(idOrdenItem);
+        return new ResponseEntity<>("Orden de item eliminada exitosamente!", HttpStatus.OK);
+    }
 }
