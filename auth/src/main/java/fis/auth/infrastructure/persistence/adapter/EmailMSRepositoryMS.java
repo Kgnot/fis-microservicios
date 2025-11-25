bad6be70de3b6f23f73ced4b18de9bed2d861cf7
Henry Ricaurte Mora <105388665+Kgnot@users.noreply.github.com>
2025-11-25 18:31:37 -0500

    Update email content type and add logging
    Change email content type from TEXT_HTML to APPLICATION_JSON and add logging for email sending.

--- auth/src/main/java/fis/auth/infrastructure/persistence/adapter/EmailMSRepositoryMS.java
+++ auth/src/main/java/fis/auth/infrastructure/persistence/adapter/EmailMSRepositoryMS.java
@@ -5,6 +5,8 @@ import fis.auth.domain.model.EmailRequest;
 import fis.auth.infrastructure.dto.response.api.ApiResponse;
 import fis.auth.infrastructure.endpoints.EmailEndpoint;
 import fis.auth.infrastructure.error.EmailApiErrorException;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
@@ -12,8 +14,8 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-
 @Repository
+@Slf4j
 public class EmailMSRepositoryMS implements EmailMSRepository {
 
     private final RestTemplate restTemplate;
@@ -22,26 +24,24 @@ public class EmailMSRepositoryMS implements EmailMSRepository {
         this.restTemplate = restTemplate;
     }
 
-
     @Override
     public void enviarEmailSignIn(EmailRequest emailRequest) {
         HttpHeaders headers = new HttpHeaders();
         headers.setBasicAuth("admin", "admin1234");
-        headers.setContentType(MediaType.valueOf(MediaType.TEXT_HTML_VALUE));
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<EmailRequest> request = new HttpEntity<>(
                 emailRequest,
-                headers
-        );
+                headers);
 
         try {
+            log.info("Enviando email a: {}", request);
             ResponseEntity<ApiResponse<Void>> response = restTemplate
                     .exchange(EmailEndpoint.POST_EMAIL_SEND.getEndpoint(),
                             HttpMethod.POST,
                             request,
                             new ParameterizedTypeReference<>() {
-                            }
-                    );
+                            });
             if (!response.getStatusCode().is2xxSuccessful()) {
                 throw EmailApiErrorException.of("Error al enviar el mensaje");
             }
