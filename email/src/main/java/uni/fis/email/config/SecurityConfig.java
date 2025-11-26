59dbc3f926d799a811917419eab93e10dfbcf45b
Giovanny Sierra <giosreina@gmail.com>
2025-11-25 19:23:24 -0500

    borre un comentario

--- email/src/main/java/uni/fis/email/config/SecurityConfig.java
+++ email/src/main/java/uni/fis/email/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             
-            .httpBasic(basic -> {});  // Habilitar HTTP Basic Authentication
+            .httpBasic(basic -> {}); 
 
         return http.build();
     }
