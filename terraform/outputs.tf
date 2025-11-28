fd4dcb0e6fa31db962c18b96404a09bdf2eb321e
Kgnot <hricaurtem@proton.me>
2025-11-28 08:07:52 -0500

    elimine terraform

--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip" {
-  value = google_compute_instance.free_vm.network_interface[0].access_config[0].nat_ip
-}
--- terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "GOOGLE_PROJECT" {
-  type = string
-}
-
-variable "GOOGLE_REGION" {
-  type = string
-  default = "us-east1"
-}
