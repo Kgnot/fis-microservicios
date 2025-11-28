15d2e8bb7e8ceedd8770fe1cc16f785f7f116dde
Kgnot <hricaurtem@proton.me>
2025-11-28 15:59:39 -0500

    actualizacion suave

--- usuario/src/main/java/uni/fis/usuario/controller/UserController.java
+++ usuario/src/main/java/uni/fis/usuario/controller/UserController.java
@@ -1,10 +1,13 @@
 package uni.fis.usuario.controller;
 
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uni.fis.usuario.dto.UserDto;
+import uni.fis.usuario.dto.request.UserRequest;
 import uni.fis.usuario.dto.response.ApiResponse;
+import uni.fis.usuario.dto.response.TokenResponse;
 import uni.fis.usuario.dto.response.UserResponse;
 import uni.fis.usuario.mapper.UserMapper;
 import uni.fis.usuario.service.UserService;
@@ -16,29 +19,33 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
-        private final UserService userService;
+    private final UserService userService;
 
-        @GetMapping("")
-        public ResponseEntity<ApiResponse<List<UserResponse>>> findAll() {
-                List<UserDto> userDto = userService.findAll();
-                return ResponseEntity.ok(
-                                ApiResponse.success(
-                                                "Usuarios encontrados",
-                                                userDto.stream()
-                                                                .map(UserMapper::dtoToResponse)
-                                                                .toList()));
-        }
+    @GetMapping("")
+    public ResponseEntity<ApiResponse<List<UserResponse>>> findAll() {
+        List<UserDto> userDto = userService.findAll();
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        "Usuarios encontrados",
+                        userDto.stream()
+                                .map(UserMapper::dtoToResponse)
+                                .toList()
+                )
+        );
+    }
 
-        @GetMapping("/{id}")
-        public ResponseEntity<ApiResponse<UserResponse>> findById(@PathVariable Integer id) {
-                return userService.findById(id)
-                                .map(userDto -> ResponseEntity.ok(
-                                                ApiResponse.success(
-                                                                "Usuario encontrado",
-                                                                UserMapper.dtoToResponse(userDto))))
-                                .orElseGet(() -> ResponseEntity.status(404).body(
-                                                ApiResponse.error("Usuario no encontrado", 404) // ← 404 en lugar de 402
-                                ));
-        }
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<UserResponse>> findById(@PathVariable Integer id) {
+        return userService.findById(id)
+                .map(userDto -> ResponseEntity.ok(
+                        ApiResponse.success(
+                                "Usuario encontrado",
+                                UserMapper.dtoToResponse(userDto)
+                        )
+                ))
+                .orElseGet(() -> ResponseEntity.status(404).body(
+                        ApiResponse.error("Usuario no encontrado", 404) // ← 404 en lugar de 402
+                ));
+    }
 
 }
